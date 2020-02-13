963543795bc345de5d32ae2d3d505693602e5c7a
theremover <theremover@moonslanding.net>
2020-02-12 23:25:28 -0600

    fixed arguments

--- init_db.sh
+++ init_db.sh
@@ -1,63 +1,95 @@
 #!/bin/bash
 
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
 read_secret()
 {
-    # Disable echo.
-    stty -echo
+  # Disable echo.
+  /bin/stty -echo
 
-    # Set up trap to ensure echo is enabled before exiting if the script
-    # is terminated while echo is disabled.
-    trap 'stty echo' EXIT
+  # Set up trap to ensure echo is enabled before exiting if the script
+  # is terminated while echo is disabled.
+  trap '/bin/stty echo' EXIT
 
-    # Read secret.
-    read "$@"
+  # Read secret.
+  read "$@"
+ 
+  # Enable echo.
+  /bin/stty echo
+  trap - EXIT
 
-    # Enable echo.
-    stty echo
-    trap - EXIT
-
-    # Print a newline because the newline entered by the user after
-    # entering the passcode is not echoed. This ensures that the
-    # next line of output begins at a new line.
-    echo
+  # Print a newline because the newline entered by the user after
+  # entering the passcode is not echoed. This ensures that the
+  # next line of output begins at a new line.
+  echo
 }
 
 help_menu()
 {
-   echo "Usage"
-   echo "  Flags"
-   echo "     --db-user, -u=Database User"
-   echo "     --path, -p=Path"
+  echo "Usage"
+  echo "  Flags"
+  echo "     --db-user, -u Database User"
+  echo "     --directory, -d Directory"
 }
 
-while [ ! $# -eq 0 ]
-do
-        case "$1" in
-                --help | -h)
-                        help_menu
-                        exit
-                        ;;
-                --user | -u)
-                        secretopt
-                        exit
-                        ;;
-        esac
+args()
+{
+  OPTS=$(getopt -q -o hd:u: -l help,directory:,user: -- "$@")
+  if [ $? != 0 ] ; then help_menu >&2 ; exit 1 ; fi
+  echo $OPTS
+  eval set -- "$OPTS"
+  while true; do
+    case "$1" in
+      --help | -h)
+        help_menu
+        exit
+        ;;
+      --directory | -d)
+        shift;
+        DIRECTORY=$1
+        ;;
+      --user | -u)
+        shift;
+        DB_USER=$1
+        ;;
+      --)
         shift
-done
+        break
+        ;;
+    esac
+    shift
+  done
+}
+      
 
-PATH=${BASEDIR}
-echo "Please enter DB User"
-read DB_USER
-echo "Please enter DB Password"
-read_secret DB_PASS
+DIRECTORY=${PWD}
 
-mkdir $PATH/guacamole_postgres_database
+args "$@"
 
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > $PATH/initdb.sql
-POSTGRES_ID=docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -v $PATH/guacamole_postgres_database:/var/lib/postgresql/data -v $PATH/initdb.sql:/initdb.sql postgres:12
-while [ "`docker inspect -f {{.State.Health.Status}} $POSTGRES_ID`" != "healthy" ]; do sleep 2; done
+if [ -z "$DB_USER" ]
+then
+  echo "Please enter DB User"
+  read DB_USER
+fi
+
+if [ -z "${DB_PASS}"]
+then
+  echo "Please enter DB Password"
+  read_secret DB_PASS
+else
+  DB_PASS=${DB_PASS}
+fi
+
+mkdir $DIRECTORY/guacamole_postgres_database
+
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > $DIRECTORY/initdb.sql
+POSTGRES_ID=$(docker run -d --rm --name pg-docker -e POSTGRES_PASSWORD=docker -v $DIRECTORY/guacamole_postgres_database:/var/lib/postgresql/data -v $DIRECTORY/initdb.sql:/initdb.sql postgres:12)
+while [ "`docker inspect -f {{.State.Status}} $POSTGRES_ID`" != "running" ]; do sleep 2; done
 docker exec -it pg-docker createdb -U postgres guacamole_db
-docker exec -it pg-docker psql -U postgres -d guacamole_db \i /initdb.sql
-docker exec -it pg-docker psql -U postgres -d guacamole_db  CREATE USER $DB_USER WITH PASSWORD "'"$DB_PASS"'";
-docker exec -it pg-docker psql -U postgres -d guacamole_db  GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO $DB_USER;
-docker exec -it pg-docker psql -U postgres -d guacamole_db GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;
+docker exec -it pg-docker psql -U postgres -d guacamole_db -c "\i /initdb.sql"
+docker exec -it pg-docker psql -U postgres -d guacamole_db  -c "CREATE USER "$DB_USER" WITH PASSWORD '"$DB_PASS"';"
+docker exec -it pg-docker psql -U postgres -d guacamole_db  -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO "$DB_USER";"
+docker exec -it pg-docker psql -U postgres -d guacamole_db -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO "$DB_USER";"
