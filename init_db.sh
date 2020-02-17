26080b674ba4a52642f64939e48282e15ff89732
theremover <theremover@moonslanding.net>
2020-02-17 08:03:16 -0600

    updated menu

--- init_db.sh
+++ init_db.sh
@@ -35,9 +35,11 @@ help_menu()
   echo "     --directory, -d Directory"
 }
 
+verbose=0
+
 args()
 {
-  OPTS=$(getopt -q -o hd:u: -l help,directory:,user: -- "$@")
+  OPTS=$(getopt -q -o vhd:u: -l verbose,help,directory:,user: -- "$@")
   if [ $? != 0 ] ; then help_menu >&2 ; exit 1 ; fi
   echo $OPTS
   eval set -- "$OPTS"
@@ -49,11 +51,15 @@ args()
         ;;
       --directory | -d)
         shift;
-        DIRECTORY=$1
+        directory=$1
         ;;
       --user | -u)
         shift;
-        DB_USER=$1
+        db_user=$1
+        ;;
+      --verbose | -v)
+        shift;
+        verbose=1
         ;;
       --)
         shift
@@ -72,24 +78,36 @@ args "$@"
 if [ -z "$DB_USER" ]
 then
   echo "Please enter DB User"
-  read DB_USER
+  read db_user
 fi
 
 if [ -z "${DB_PASS}"]
 then
   echo "Please enter DB Password"
-  read_secret DB_PASS
+  read_secret db_pass
 else
-  DB_PASS=${DB_PASS}
+  db_pass=${DB_PASS}
 fi
 
-mkdir $DIRECTORY/guacamole_postgres_database
+echo "Creating Postgress Database folder in $directory"
+mkdir $directory/guacamole_postgres_database
 
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > $DIRECTORY/initdb.sql
-POSTGRES_ID=$(docker run -d --rm --name pg-docker -e POSTGRES_PASSWORD=docker -v $DIRECTORY/guacamole_postgres_database:/var/lib/postgresql/data -v $DIRECTORY/initdb.sql:/initdb.sql postgres:12)
-while [ "`docker inspect -f {{.State.Status}} $POSTGRES_ID`" != "running" ]; do sleep 2; done
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > $directory/initdb.sql
+echo "Starting Postgress in Docker"
+if [[ -z "$verbose" ]] ; then
+    exec 2>&1 >/dev/null
+fi
+postgress_id=$(docker run -d --rm --name pg-docker -e POSTGRES_PASSWORD=docker -v $directory/guacamole_postgres_database:/var/lib/postgresql/data -v $directory/initdb.sql:/initdb.sql postgres:12)
+trap "docker stop $postgress_id" EXIT
+exec > /dev/stdout
+echo "Initializing database"
+if [[ -z "$verbose" ]] ; then
+    exec 2>&1 >/dev/null
+fi
+while [ "`docker inspect -f {{.State.Status}} $postgress_id`" != "running" ]; do sleep 2; done
 docker exec -it pg-docker createdb -U postgres guacamole_db
 docker exec -it pg-docker psql -U postgres -d guacamole_db -c "\i /initdb.sql"
-docker exec -it pg-docker psql -U postgres -d guacamole_db  -c "CREATE USER "$DB_USER" WITH PASSWORD '"$DB_PASS"';"
-docker exec -it pg-docker psql -U postgres -d guacamole_db  -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO "$DB_USER";"
-docker exec -it pg-docker psql -U postgres -d guacamole_db -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO "$DB_USER";"
+docker exec -it pg-docker psql -U postgres -d guacamole_db  -c "CREATE USER "$db_user" WITH PASSWORD '"$db_pass"';"
+docker exec -it pg-docker psql -U postgres -d guacamole_db  -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO "$db_user";"
+docker exec -it pg-docker psql -U postgres -d guacamole_db -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO "$db_user";"
+docker stop $postgress_id
